b37e861222c7fc48b6497b72bc40d5c6d69e18f5
Hieu Bui <hieu.bui@anphabe.com>
2022-12-29 17:01:07 +0700

    update

--- src/components/LinkSheet/linkSS.js
+++ src/components/LinkSheet/linkSS.js
@@ -27,12 +27,12 @@ const App = () => {
 
         <div className="element" key={pase["STT"].toString()}>
             < div className="title" > {pase["Title"]} </div >
-
-            <input type="text" value={pase["Link"]} id={parseInt(pase["STT"])} style={{ display: 'none' }} readOnly></input>
-            <a href={pase["Link"]}>{pase["Link"]}</a>
-
+            <p>
+                <input type="text" value={pase["Link"]} id={parseInt(pase["STT"])} style={{ display: 'none' }} readOnly></input>
+                <a href={pase["Link"]}>{pase["Link"]}</a>
+            </p>
             <div className="tooltip">
-                <button className="button-4" onClick={() =>  navigator.clipboard.writeText(pase["Link"])} onMouseOut={outFunc(pase["STT"])} >
+                <button className="button-4" onClick={() => navigator.clipboard.writeText(pase["Link"])} onMouseOut={outFunc(pase["STT"])} >
                     <span className="tooltiptext" id={"myTooltip" + pase["STT"]}>Copy to clipboard</span>
                     Copy Link
                 </button>
