de2ba368561002b0898b9b0775a969a5d83eccc5
Hieu Bui <hieu.bui@anphabe.com>
2022-12-30 15:32:07 +0700

    add render function

--- src/components/LinkSheet/linkPASE.js
+++ src/components/LinkSheet/linkPASE.js
@@ -1,7 +1,6 @@
 import "../../css/main.css";
 import useGoogleSheets from 'use-google-sheets';
-import { outFunc } from '../utils/copyButton';
-
+import  renderItem  from "./renderItem";
 //sheetsOptions: [{ id: 'Sheet1' }],
 const App = () => {
     const { data, loading, error } = useGoogleSheets({
@@ -23,21 +22,10 @@ const App = () => {
     <div class="segment">
     </div>
     const listItems = arr[0]['data'].map((pase) =>
+            <div>
+            {renderItem(pase)}
+            </div>       
 
-        <div className="element" key={pase["STT"].toString()}>
-            < div className="title" > {pase["Title"]} </div >
-            <p>
-            <input type="text" value={pase["Link"]} id={parseInt(pase["STT"])} style={{ display: 'none' }} readOnly></input>
-            <a href={pase["Link"]}>{pase["Link"]}</a>
-            </p>
-            <div className="tooltip">
-                <button className="button-4" onClick={() =>  navigator.clipboard.writeText(pase["Link"])} onMouseOut={outFunc(pase["STT"])} >
-                    <span className="tooltiptext" id={"myTooltip" + pase["STT"]}>Copy to clipboard</span>
-                    Copy Link
-                </button>
-            </div>
-            <textarea readOnly disabled defaultValue={pase["Description"].replace(/^\s+|\s+$/gm, '').trim()}></textarea>
-        </div>
     );
     return (
         <div className="X">
--- src/components/LinkSheet/renderItem.js
+++ src/components/LinkSheet/renderItem.js
@@ -1,22 +1,26 @@
 import "../../css/main.css";
 import { outFunc } from '../utils/copyButton';
 
-
 const renderItem = (pase) => {
-    <div className="element" key={pase["STT"].toString()}>
-        < div className="title" > {pase["Title"]} </div >
-
-        <input type="text" value={pase["Link"]} id={parseInt(pase["STT"])} style={{ display: 'none' }} readOnly></input>
-        <a href={pase["Link"]}>{pase["Link"]}</a>
-
-        <div className="tooltip">
-            <button className="button-4" onClick={() => navigator.clipboard.writeText(pase["Link"])} onMouseOut={outFunc(pase["STT"])} >
-                <span className="tooltiptext" id={"myTooltip" + pase["STT"]}>Copy to clipboard</span>
-                Copy Link
-            </button>
+    return (
+        <div className="element" key={pase["STT"].toString()}>
+            < div className="title" > {pase["Title"]} </div >
+            <p>
+                <input type="text" value={pase["Link"]} id={parseInt(pase["STT"])} style={{ display: 'none' }} readOnly></input>
+                <a href={pase["Link"]}>{pase["Link"]}</a>
+            </p>
+            <div className="tooltip">
+                <button className="button-4" onClick={() => navigator.clipboard.writeText(pase["Link"])} onMouseOut={outFunc(pase["STT"])} >
+                    <span className="tooltiptext" id={"myTooltip" + pase["STT"]}>Copy to clipboard</span>
+                    Copy Link
+                </button>
+            </div>
+            <textarea readOnly disabled defaultValue={pase["Description"].replace(/^\s+|\s+$/gm, '').trim()}></textarea>
+            <p>
+                Tags: {pase["Tags"]}
+            </p>
         </div>
-        <textarea readOnly disabled defaultValue={pase["Description"].replace(/^\s+|\s+$/gm, '').trim()}></textarea>
-    </div>
+    )
 }
 
-export default renderItem;
+export default renderItem ;
